c2339678107d737b8af7bb5f242e5c3548d2b2f2
William L. Marr III <hobbyist@wlmiii.net>
2016-04-25 17:15:50 -0400

    Create the data model for Pokemon objects

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by William L. Marr III on 4/25/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+}
