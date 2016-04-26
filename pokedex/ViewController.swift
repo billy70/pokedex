b829ef232a80ca675058f688f8d3e0d9b2ca0e77
William L. Marr III <hobbyist@wlmiii.net>
2016-04-25 21:56:44 -0400

    Add the data source and delegates for the collection view
    Note that this commit includes sample data for testing that the collection view is working.

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collection: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collection.dataSource = self
+        collection.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +27,39 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("pokemonCell", forIndexPath: indexPath) as? PokeCell {
+            
+            let pokemon = Pokemon(name: "TEST_\(indexPath.row)", pokedexID: indexPath.row + 1)
+            cell.configureCell(pokemon)
+            
+            return cell
+            
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 718
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(105, 105)
+    }
+}
