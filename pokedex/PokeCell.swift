c1230534fa34e2b0581636c6cae246aaa366cd61
William L. Marr III <hobbyist@wlmiii.net>
2016-04-25 21:15:09 -0400

    Create custom PokeCell class and connect outlets

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by William L. Marr III on 4/25/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalizedString
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+}
