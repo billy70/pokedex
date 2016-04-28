93bf841a90db0813bdbb82bd6d20009cae60c09e
William L. Marr III <hobbyist@wlmiii.net>
2016-04-27 20:54:01 -0400

    Filter the collection view based on the search criteria

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -18,12 +18,16 @@ class ViewController: UIViewController {
     var musicPlayer: AVAudioPlayer!
     let volumeImageOFF = UIImage(named: "volume-OFF.png")
     let volumeImageON = UIImage(named: "volume-ON.png")
+    var inSearchMode = false
+    var filteredPokemon = [Pokemon]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         collection.dataSource = self
         collection.delegate = self
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done
         
         initAudio()
         parsePokemonCSV()
@@ -87,7 +91,15 @@ extension ViewController: UICollectionViewDataSource {
         
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("pokemonCell", forIndexPath: indexPath) as? PokeCell {
             
-            let poke = pokemon[indexPath.row]
+            //let poke = pokemon[indexPath.row]
+            let poke: Pokemon!
+            
+            if inSearchMode {
+                poke = filteredPokemon[indexPath.row]
+            } else {
+                poke = pokemon[indexPath.row]
+            }
+            
             cell.configureCell(poke)
             
             return cell
@@ -102,7 +114,11 @@ extension ViewController: UICollectionViewDataSource {
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        if inSearchMode {
+            return filteredPokemon.count
+        }
+        
+        return pokemon.count
     }
 }
 
@@ -115,5 +131,31 @@ extension ViewController: UICollectionViewDelegate {
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSizeMake(105, 105)
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            view.endEditing(true)
+        } else {
+            inSearchMode = true
+            
+            let lower = searchBar.text!.lowercaseString
+            filteredPokemon = pokemon.filter( {$0.name.rangeOfString(lower) != nil} )
+        }
+
+        collection.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        inSearchMode = false
+        view.endEditing(true)
+        collection.reloadData()
     }
 }
