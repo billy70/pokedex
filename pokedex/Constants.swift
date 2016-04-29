1e211f993f00c6d027c1b9e7915d14031bac8cc0
William L. Marr III <hobbyist@wlmiii.net>
2016-04-29 13:46:47 -0400

    Add source file for project-global constants

---
+++ pokedex/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by William L. Marr III on 4/29/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMONAPI_V1 = "/api/v1/pokemon/"
